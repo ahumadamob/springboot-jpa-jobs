c08c532269005ab722e591bf70bc75ff8c609d19
Mario Ahumada <ahumadamob@gmail.com>
2023-02-07 17:26:34 -0300

    78

---
+++ src/main/java/com/ahumadamob/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ahumadamob.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ahumadamob.model.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+
+}
