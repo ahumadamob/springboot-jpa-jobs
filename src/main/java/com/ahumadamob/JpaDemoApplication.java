566c4a97040ee3bd927b109664afa3e2b47278c7
Mario Ahumada <ahumadamob@gmail.com>
2023-02-07 18:00:40 -0300

    79

--- src/main/java/com/ahumadamob/JpaDemoApplication.java
+++ src/main/java/com/ahumadamob/JpaDemoApplication.java
@@ -1,19 +1,25 @@
 package com.ahumadamob;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ahumadamob.repository.CategoryRepository;
+
 @SpringBootApplication
 public class JpaDemoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CategoryRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Ejemplo SpringBoot JPA");
+		System.out.println(repo);
 		
 	}
 
